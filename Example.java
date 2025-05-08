0be5333ca7a2d14aab3b8fdd51a82e3937abaec8
keerthika03-A <keerthikaarumugam36@gmail.com>
2025-05-08 12:37:27 +0530

    Delete Example.java

--- Example.java
+++
@@ -1,8 +0,0 @@
-class Example {
-    public static void main(String[] args) {
-        MCA_2024 mca = new MCA_2024();
-        mca.java();
-        mca.softwareEngineering();
-        mca.operatingSystem();
-    }
-}
